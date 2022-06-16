5070c160b04d818895bff552517a868b7db84e21
Pravesh22 <budhathokipravesh5@gmail.com>
2022-06-16 10:01:27 +0545

    setup modified

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
-# Copyright (c) Megvii, Inc. and its affiliates. All Rights Reserved
-
+"""
+-- Created by Pravesh Budhathoki
+-- Treeleaf Technologies Pvt. Ltd.
+-- Created on 2022-06-15
+"""
 import re
-import setuptools
 import sys
+from setuptools import find_packages, setup
 
 TORCH_AVAILABLE = True
 try:
@@ -61,21 +63,17 @@ def get_cmd_class():
     return cmdclass
 
 
-setuptools.setup(
+setup(
     name="yolox",
-    version=get_yolox_version(),
-    author="megvii basedet team",
+    version="1.0",
+    author="Pravesh Kaji Budhathoki",
     url="https://github.com/Megvii-BaseDetection/YOLOX",
     package_dir=get_package_dir(),
-    packages=setuptools.find_packages(exclude=("tests", "tools")) + list(get_package_dir().keys()),
-    python_requires=">=3.6",
-    install_requires=get_install_requirements(),
-    setup_requires=["wheel"],  # avoid building error when pip is not updated
+    packages=find_packages(exclude=("tests", "tools")) + list(get_package_dir().keys()),
+    python_requires=">=3.6",  # avoid building error when pip is not updated
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     include_package_data=True,  # include files in MANIFEST.in
-    ext_modules=get_ext_modules(),
-    cmdclass=get_cmd_class(),
     classifiers=[
         "Programming Language :: Python :: 3", "Operating System :: OS Independent",
         "License :: OSI Approved :: Apache Software License",
